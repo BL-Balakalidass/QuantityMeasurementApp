bb76ba1496e3705df254410b41f5dafa8cab71c9
BL-Balakalidass <bala.kalidass@bridgelabz.com>
2026-06-23 10:13:09 +0530

    [Bala] : UC2 - Feet and Inches measurement equality

--- QuantityMeasurement/src/main/java/QuantityMeasurementApp.java
+++ QuantityMeasurement/src/main/java/QuantityMeasurementApp.java
@@ -27,11 +27,61 @@ public class QuantityMeasurementApp {
         }
     }
 
+    public static class Inches {
+
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            Inches other = (Inches) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static boolean checkFeetEquality(
+            double firstValue,
+            double secondValue) {
+
+        Feet firstFeet = new Feet(firstValue);
+        Feet secondFeet = new Feet(secondValue);
+
+        return firstFeet.equals(secondFeet);
+    }
+
+    public static boolean checkInchesEquality(
+            double firstValue,
+            double secondValue) {
+
+        Inches firstInches = new Inches(firstValue);
+        Inches secondInches = new Inches(secondValue);
+
+        return firstInches.equals(secondInches);
+    }
+
     public static void main(String[] args) {
 
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(1.0);
+        System.out.println(
+                "Feet Equality : "
+                        + checkFeetEquality(1.0, 1.0)
+        );
 
-        System.out.println("Equal: " + feet1.equals(feet2));
+        System.out.println(
+                "Inches Equality : "
+                        + checkInchesEquality(1.0, 1.0)
+        );
     }
 }
